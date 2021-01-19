2be985e3a53d5167f97c4d325d78224ceab02521
Joshua Alley <jauschalley@gmail.com>
2021-01-19 14:41:44 -0500

    basics of react and destructuring props

---
+++ src/App.js
@@ -0,0 +1,25 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ])
+}
+
+const App = () => {
+  return React.createElement(
+    "div", //target
+    {}, // attributes to be passed in
+    [
+      React.createElement("h1", {}, "Adopt Me!"), //children
+      React.createElement(Pet, { name: "Ben", animal: "Dog", breed: "Wheaten" }),
+      React.createElement(Pet, { name: "Frank", animal: "Dog", breed: "Doodle" }),
+      React.createElement(Pet, { name: "Winston", animal: "Dog", breed: "Beagle" })
+    ]
+  )
+}
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("root")
+);
