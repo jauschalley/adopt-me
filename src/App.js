dbd8afb24ee44a5d22815e69a0058cd98ab348b9
Joshua Alley <jauschalley@gmail.com>
2021-01-19 16:26:41 -0500

    babys first react hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+// import Pet from './Pet';
+import SearchParams from './SearchParams'
 
 const App = () => {
-  return React.createElement(
-    "div", //target
-    {}, // attributes to be passed in
-    [
-      //children
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Ben",
-        animal: "Dog",
-        breed: "Wheaten",
-      }),
-      React.createElement(Pet, {
-        name: "Frank",
-        animal: "Dog",
-        breed: "Doodle",
-      }),
-      React.createElement(Pet, {
-        name: "Winston",
-        animal: "Dog",
-        breed: "Beagle",
-      }),
-    ]
-  );
+  return (
+    <SearchParams/>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
